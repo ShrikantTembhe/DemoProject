bccd9012c2996d95007c066e3f01bcbc1eba9139
SHREE <shrikanttembhe85@gmail.com>
2023-12-20 20:27:44 +0530

    MultiplicationNumbers

---
+++ JavaGitProject/src/com/github/java/MultiplyNumbers.java
@@ -0,0 +1,12 @@
+package com.github.java;
+
+public class MultiplyNumbers {
+
+	public static void main(String args[]) {
+
+		int a = 15;
+		int b = 6;
+		int Result = a * b;
+		System.out.println(Result);
+	}
+}
