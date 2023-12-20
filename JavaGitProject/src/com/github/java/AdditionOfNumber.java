7168693095770b5bbe563a85c4ec1418643ba0c0
SHREE <shrikanttembhe85@gmail.com>
2023-12-20 15:25:46 +0530

    Addition of numbers

---
+++ JavaGitProject/src/com/github/java/AdditionOfNumber.java
@@ -0,0 +1,12 @@
+package com.github.java;
+
+public class AdditionOfNumber {
+
+	public static void main(String args[]) {
+
+		int a = 10;
+		int b = 15;
+		int Result = a + b;
+		System.out.println(Result);
+	}
+}
