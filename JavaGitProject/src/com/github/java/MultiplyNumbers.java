dd3e4fc52d299e7a3550db1c4755c2226621295d
ShrikantTembhe <154218003+ShrikantTembhe@users.noreply.github.com>
2023-12-20 20:41:05 +0530

    Update MultiplyNumbers.java

--- JavaGitProject/src/com/github/java/MultiplyNumbers.java
+++ JavaGitProject/src/com/github/java/MultiplyNumbers.java
@@ -1,12 +1,23 @@
 package com.github.java;
-
+import java.util.Scanner;
 public class MultiplyNumbers {
-
+        public static void getDivision(float c ,float d)
+	{
+		float div = c/d;
+		System.out.println("Division is:"+div)
+	}
 	public static void main(String args[]) {
 
 		int a = 15;
 		int b = 6;
 		int Result = a * b;
 		System.out.println(Result);
+		//Addinng the another functionality as division of numbers
+	      Scanner sobj = new Scanner(System.in);
+		System.out.println("Enter first number:")
+			float c =sobj.nextFloat();
+		System.out.println("Enter second number:")
+			float d = sobj.nextFloat();
+		getDivision(c,d);
 	}
 }
